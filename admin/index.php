2f9da670e01fe813204f4f7652acb5a314190233
tronicboone <e@sixtrill.com>
2014-06-08 19:53:33 -0400

    Initial commit

---
+++ admin/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>E's Blue Note Record Collection - Admin</title>
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+	<link rel="stylesheet" href="css/app.css" />  
+</head>
+<body>
+<?php  
+
+
+
+?>
+
+	
+<form class="pure-form pure-form-aligned">
+	<fieldset>
+	    <div class="pure-control-group">
+	        <label for="name">Artist</label>
+	        <input id="name" type="text" placeholder="Artist Name">
+	    </div>
+
+	    <div class="pure-control-group">
+	        <label for="password"></label>
+	        <input id="password" type="password" placeholder="Password">
+	    </div>
+
+	    <div class="pure-control-group">
+	        <label for="email">Email Address</label>
+	        <input id="email" type="email" placeholder="Email Address">
+	    </div>
+
+	    <div class="pure-control-group">
+	        <label for="foo">Supercalifragilistic Label</label>
+	        <input id="foo" type="text" placeholder="Enter something here...">
+	    </div>
+
+	    <div class="pure-controls">
+	        <label for="cb" class="pure-checkbox">
+	            <input id="cb" type="checkbox"> I've read the terms and conditions
+	        </label>
+
+	        <button type="submit" class="pure-button pure-button-primary">Submit</button>
+	    </div>
+	</fieldset>
+</form>
+</body>
+</html>
