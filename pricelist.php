ea67a24048c7bdf090e2d30fc5ed7f133349f82a
tronicboone <e@sixtrill.com>
2014-06-08 19:52:22 -0400

    Initial commit

---
+++ pricelist.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>E's Blue Note Record Collection - Prices</title>
+  <link rel="stylesheet" href="css/app.css" />
+  <script src="bower_components/modernizr/modernizr.js"></script>
+</head>
+<body>
+<?php  
+
+
+
+?>
+</body>
+</html>
