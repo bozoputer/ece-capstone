8d465e341dbd08e41b56be87036d1645dca40b6d
tronicboone <e@sixtrill.com>
2014-06-12 18:02:35 -0400

    Add price table to page via MySQL db

--- pricelist.php
+++ pricelist.php
@@ -8,36 +8,40 @@
   <script src="bower_components/modernizr/modernizr.js"></script>
 </head>
 <body>  
-<?php
+<div class="row">
+	<div class="small-12 columns" style="border: 1px solid red;">
+		<?php
 
-include('../ext_includes/capstone.inc.php');
+		include('../ext_includes/capstone.inc.php');
 
-$result = mysqli_query($con,"SELECT * FROM records");
+		$result = mysqli_query($con,"SELECT * FROM records");
 
-echo "<table>
-<tr>
-<th>Artist</th>
-<th>Title</th>
-<th>Year</th>
-<th>Status</th>
-<th>Price</th>
-<th>Condition</th>
-</tr>";
+		echo "<table>
+		<tr>
+		<th>Artist</th>
+		<th>Title</th>
+		<th>Year</th>
+		<th>Status</th>
+		<th>Price</th>
+		<th>Condition</th>
+		</tr>";
 
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  echo "<td>" . $row['artist'] . "</td>";
-  echo "<td>" . $row['title'] . "</td>";
-  echo "<td>" . $row['year'] . "</td>";
-  echo "<td>" . $row['status'] . "</td>";
-  echo "<td>" . $row['price'] . "</td>";
-  echo "<td>" . $row['condition'] . "</td>";
-  echo "</tr>";
-}
+		while($row = mysqli_fetch_array($result)) {
+		  echo "<tr>";
+		  echo "<td>" . $row['artist'] . "</td>";
+		  echo "<td>" . $row['title'] . "</td>";
+		  echo "<td>" . $row['year'] . "</td>";
+		  echo "<td>" . $row['status'] . "</td>";
+		  echo "<td>" . $row['price'] . "</td>";
+		  echo "<td>" . $row['condition'] . "</td>";
+		  echo "</tr>";
+		}
 
-echo "</table>";
+		echo "</table>";
 
-mysqli_close($con);
-?> 
+		mysqli_close($con);
+		?> 
+	</div>
+</div>
 </body>
 </html>
