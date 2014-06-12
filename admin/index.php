0306f7f8841a2990156b12b9e278f6d3d2ddf56a
tronicboone <e@sixtrill.com>
2014-06-12 17:57:49 -0400

    Create from to add record to database

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>E's Blue Note Record Collection - Admin</title>
 	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
-	<link rel="stylesheet" href="css/app.css" />  
 </head>
 <body>
 <?php  
