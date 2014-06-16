def2820d072a88cb3914a215c9aac6c0beb65658
tronicboone <e@sixtrill.com>
2014-06-16 12:24:10 -0400

    Add client-side validation

--- admin/index.php
+++ admin/index.php
@@ -20,22 +20,25 @@
 	<fieldset>
 	    <div class="pure-control-group">
 	        <label for="artist">Artist</label>
-	        <input id="artist" type="text" placeholder="Artist Name" required>
+	        <input id="artist" type="text" placeholder="Artist Name">
+	        <span id="errorMsg"/>
 	    </div>
 
 	    <div class="pure-control-group">
 	        <label for="album">Album</label>
-	        <input id="album" type="text" placeholder="Album Title" required>
+	        <input id="album" type="text" placeholder="Album Title">
+	        <span id="errorMsg"/>
 	    </div>
 
 	    <div class="pure-control-group">
 	        <label for="year">Year</label>
-	        <input id="year" type="text" placeholder="Release Year" required>
+	        <input id="year" type="text" placeholder="Release Year">
+	        <span id="errorMsg"/>
 	    </div>
 
 	    <div class="pure-control-group">
 	        <label for="status">Status</label>
-	        <select id="status" required>
+	        <select id="status">
 	            <option>Buying</option>
 	            <option>Selling</option>
 	        </select>
@@ -43,48 +46,57 @@
 
 	    <div class="pure-control-group">
 	        <label for="price">Price</label>
-	        <input id="price" type="text" placeholder="Price" required>
+	        <input id="price" type="text" placeholder="Price">
+	        <span id="errorMsg"/>
 	    </div>
 
 	    <div class="pure-control-group">
 	        <label for="condition">Condition</label>
-	        <input id="condition" type="text" placeholder="Condition" required>
+	        <input id="condition" type="text" placeholder="Condition">
+	        <span id="errorMsg"/>
 	    </div>
 
 	    <div class="pure-controls">
-	        <button type="submit" value="Validate!" class="pure-button pure-button-primary">Add album</button>
+	        <button type="button" id="addAlbum" class="pure-button pure-button-primary">Add album</button>
 	    </div>
 	</fieldset>
 </form>
 </div>
 <script src="//code.jquery.com/jquery-1.9.0.min.js"></script>
-<script src="js/jquery.validate.min.js"></script>
-<script src="js/additional-methods.min.js"></script>
 <script>
-jQuery.validator.setDefaults({
-debug: true,
-success: "valid"
+$(document).ready(function() {
+	$("#artist").focus();
+	$("#addAlbum").click(function() {
+		var artist = $("#artist").val();
+		var album = $("#album").val();
+		var year = $("#year").val();
+		var price = $("#price").val();
+		var pricePattern = new RegExp (/^([1-9]{1}[\d]{0,2}(\,[\d]{3})*(\.[\d]{0,2})?|[1-9]{1}[\d]{0,}(\.[\d]{0,2})?|0(\.[\d]{0,2})?|(\.[\d]{1,2})?)$/)
+		var priceResult = price.match(pricePattern);
+		var condition = $("#condition").val().toLowerCase();
+
+		if (artist, album, year, price, condition == "") {
+			alert("Please fill out all fields.");
+		}
+		else if (year < 1939 || year > 2014)  {
+			alert("Invalid year for Blue Note Records. Please try again");
+		}
+		else if (year.length !== 4 || isNaN(year)) {
+			alert("Invalid year. Please try again");
+		}
+		else if (priceResult == null) {
+			alert("Invalid price. Please try again");
+		}
+		else if (condition !== "nm" && condition !== "e" && condition !== "vg" && condition !== "g" && condition !== "f" && condition !== "p") {
+			alert("Condition must be NM, E, VG, G, F, or P. Please try again");
+		} 
+		else {
+			$("#addAlbum").submit();
+		}
+	});
 });
 
-$("#addRecord").validate({
-	rules: {
-		artist: {
-			required: true
-		},
-		title: {
-			required: true
-		},
-		year: {
-			required: true
-		},
-		price: {
-			required: true
-		},
-		condition: {
-			required: true
-		}
-	}
-});
+
 </script>
 </body>
 </html>
