2ed9e59a351e0d68bd2590b2fe85582461aab304
tronicboone <e@sixtrill.com>
2014-06-19 19:41:25 -0400

    Finish add record to db functionality

--- admin/add_rec.php
+++ admin/add_rec.php
@@ -1,3 +1,39 @@
-<?php 
+<?php  
+require ('../../ext_includes/capstone2.inc.php');
+$db = mysqli_connect($host, $user_name, $password, $db);
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+//Get the form fields.
+$artist = ($_POST['artist']);
+$title = ($_POST['title']);
+$year = ($_POST['year']);
+$status = ($_POST['status']);
+$price = ($_POST['price']);
+$shape = ($_POST['shape']);
+
+
+//Make the fields safe.
+$artist = $db->escape_string($artist);
+$title = $db->escape_string($title);
+$year = $db->escape_string($year);
+$status = $db->escape_string($status);
+$price = $db->escape_string($price);
+$shape = $db->escape_string($shape);
+
+//Create and run the SQL.
+$sql = "INSERT INTO records (artist, title, year, status, price, shape) VALUES  ('$artist', '$title', '$year', '$status', '$price', '$shape')";
+
+if (!mysqli_query($db,$sql)) {
+  die('Error: ' . mysqli_error($db));
+}
+echo "1 record added ";
+echo $sql;
+
+mysqli_close($db);
 
 ?>
--- admin/index.php
+++ admin/index.php
@@ -8,37 +8,30 @@
 	<link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
-<?php  
 
-
-
-?>
 
 <div id="addRecord">
 <h3>Add new album to price table</h3>
-<form class="pure-form pure-form-aligned">
+<form class="pure-form pure-form-aligned" method="POST" action="add_rec.php">
 	<fieldset>
 	    <div class="pure-control-group">
 	        <label for="artist">Artist</label>
-	        <input id="artist" type="text" placeholder="Artist Name">
-	        <span id="errorMsg"/>
+	        <input id="artist" type="text" name="artist" placeholder="Artist Name">
 	    </div>
 
 	    <div class="pure-control-group">
-	        <label for="album">Album</label>
-	        <input id="album" type="text" placeholder="Album Title">
-	        <span id="errorMsg"/>
+	        <label for="title">Album</label>
+	        <input id="title" name="title" type="text" placeholder="Album Title">
 	    </div>
 
 	    <div class="pure-control-group">
 	        <label for="year">Year</label>
-	        <input id="year" type="text" placeholder="Release Year">
-	        <span id="errorMsg"/>
+	        <input id="year" name="year" type="text" placeholder="Release Year">
 	    </div>
 
 	    <div class="pure-control-group">
 	        <label for="status">Status</label>
-	        <select id="status">
+	        <select id="status" name="status">
 	            <option>Buying</option>
 	            <option>Selling</option>
 	        </select>
@@ -46,14 +39,12 @@
 
 	    <div class="pure-control-group">
 	        <label for="price">Price</label>
-	        <input id="price" type="text" placeholder="Price">
-	        <span id="errorMsg"/>
+	        <input id="price" name="price" type="text" placeholder="Price">
 	    </div>
 
 	    <div class="pure-control-group">
-	        <label for="condition">Condition</label>
-	        <input id="condition" type="text" placeholder="Condition">
-	        <span id="errorMsg"/>
+	        <label for="shape">Shape</label>
+	        <input id="shape" name="shape" type="text" placeholder="Record condition">
 	    </div>
 
 	    <div class="pure-controls">
@@ -68,14 +59,14 @@ $(document).ready(function() {
 	$("#artist").focus();
 	$("#addAlbum").click(function() {
 		var artist = $("#artist").val();
-		var album = $("#album").val();
+		var title = $("#title").val();
 		var year = $("#year").val();
 		var price = $("#price").val();
 		var pricePattern = new RegExp (/^([1-9]{1}[\d]{0,2}(\,[\d]{3})*(\.[\d]{0,2})?|[1-9]{1}[\d]{0,}(\.[\d]{0,2})?|0(\.[\d]{0,2})?|(\.[\d]{1,2})?)$/)
 		var priceResult = price.match(pricePattern);
-		var condition = $("#condition").val().toLowerCase();
+		var shape = $("#shape").val().toLowerCase();
 
-		if (artist, album, year, price, condition == "") {
+		if (artist, title, year, price, shape == "") {
 			alert("Please fill out all fields.");
 		}
 		else if (year < 1939 || year > 2014)  {
@@ -87,15 +78,14 @@ $(document).ready(function() {
 		else if (priceResult == null) {
 			alert("Invalid price. Please try again");
 		}
-		else if (condition !== "nm" && condition !== "e" && condition !== "vg" && condition !== "g" && condition !== "f" && condition !== "p") {
-			alert("Condition must be NM, E, VG, G, F, or P. Please try again");
+		else if (shape !== "nm" && shape !== "e" && shape !== "vg" && shape !== "g" && shape !== "f" && shape !== "p") {
+			alert("Shape must be NM, E, VG, G, F, or P. Please try again");
 		} 
 		else {
-			$("#addAlbum").submit();
+			$("form").submit();
 		}
 	});
 });
-
 
 </script>
 </body>
