520643efe4ccae29d586722753a42c19416b40ba
tronicboone <e@sixtrill.com>
2014-06-21 05:44:59 -0400

    Create modal for album price list

--- selling.php
+++ selling.php
@@ -141,7 +141,9 @@
             </h5>
 
             <div class="show-for-small" align="center">
-              <a href="#" class="small radius button">Call To Action!</a><br>
+              <a href="#" class="small radius button" data-reveal-id="pricelist">Call To Action!</a>
+
+              <br>
 
               <a href="#" class="small radius button">Call To Action!</a>
             </div>
@@ -157,13 +159,52 @@
 
       <div class="large-4 columns hide-for-small">
         <h4>Get In Touch!</h4><hr/>
-        <a class="large button expand" href="#">Call To Action!</a>
+        <a class="large button expand" href="#" data-reveal-id="pricelist">Call To Action!</a>
         <a class="large button expand" href="#">Call To Action!</a>
       </div>
     </div>
   </div>
 </div>
 
+<!-- Foundation Reveal - Price list modal -->
+<div id="pricelist" class="reveal-modal" data-reveal>
+    <?php
+
+    require'../ext_includes/capstone2.inc.php';
+    $db = mysqli_connect($host, $user_name, $password, $db);
+
+    // Check connection
+    if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    $result = mysqli_query($db,"SELECT * FROM records");
+
+    echo "<table>
+    <tr>
+    <th>Artist</th>
+    <th>Title</th>
+    <th>Year</th>
+    <th>Price</th>
+    <th>Condition</th>
+    </tr>";
+
+    while($row = mysqli_fetch_array($result)) {
+      echo "<tr>";
+      echo "<td>" . $row['artist'] . "</td>";
+      echo "<td>" . $row['title'] . "</td>";
+      echo "<td>" . $row['year'] . "</td>";
+      echo "<td>" . $row['price'] . "</td>";
+      echo "<td>" . $row['shape'] . "</td>";
+      echo "</tr>";
+    }
+
+    echo "</table>";
+
+    mysqli_close($db);
+    ?> 
+  <a class="close-reveal-modal">&#215;</a>
+</div>
     
   <script src="bower_components/jquery/dist/jquery.min.js"></script>
   <script src="bower_components/foundation/js/foundation.min.js"></script>
