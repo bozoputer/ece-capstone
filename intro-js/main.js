4aaed23053faab168426e749b49e4ae665778999
tronicboone <e@sixtrill.com>
2014-06-21 07:55:19 -0400

    Edit include js URL

--- intro-js/main.js
+++ intro-js/main.js
@@ -1,6 +1,6 @@
 //----Include-Function----
 function include(url){
- document.write('<script type="text/javascript" src="js/'+ url + '"></script>');
+ document.write('<script type="text/javascript" src="intro-js/'+ url + '"></script>');
  return false ;
 }
 
