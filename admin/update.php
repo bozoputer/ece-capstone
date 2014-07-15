6695ac012ac4d5d44379c6c27612988bc8612ba1
tronicboone <e@sixtrill.com>
2014-07-15 12:48:28 -0400

    Add db connection

--- admin/update.php
+++ admin/update.php
@@ -1,3 +1,14 @@
+<?php  
+require ('../../ext_includes/capstone2.inc.php');
+$db = mysqli_connect($host, $user_name, $password, $db);
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+?>
+
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
