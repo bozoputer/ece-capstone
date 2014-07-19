de37fba8ff651dac400687ceadad6ce592ab41fd
tronicboone <e@sixtrill.com>
2014-07-19 10:01:22 -0400

    Add Foundation framework

--- admin/conf-delete.php
+++ admin/conf-delete.php
@@ -18,3 +18,22 @@ echo "<p>Are you sure you want to delete " . $row['title'] . " from the database
 
 mysqli_close($db);
 ?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>E's Blue Note Record Collection - About</title>
+  <link rel="stylesheet" href="../css/app.css" />
+  <link rel="stylesheet" href="../css/styles.css" />
+  <link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+  <script src="../bower_components/modernizr/modernizr.js"></script>
+</head>
+<body>
+
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<script src="../bower_components/foundation/js/foundation.min.js"></script>
+<script src="../js/app.js"></script>
+</body>
+</html>
