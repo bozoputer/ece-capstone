1ebadf90e521ccdb86fd5faf138ed7d8668817e2
tronicboone <e@sixtrill.com>
2014-08-06 08:20:57 -0400

    Add Foundation; minor PHP tweaks

--- php/contact-send.php
+++ php/contact-send.php
@@ -1,3 +1,56 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>E's Blue Note Record Collection - About</title>
+  <link rel="stylesheet" href="../css/app.css" />
+  <link rel="stylesheet" href="../css/styles.css" />
+  <link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+  <script src="../bower_components/modernizr/modernizr.js"></script>
+  <style>
+  body {
+  	color: #cd005e;
+  	font-family: 'Oswald', Arial, sans-serif;
+	font-size: 20px;
+  }
+
+  div#errors {
+  	padding-left: 10px;
+  	padding-top: 10px;
+  }
+
+  </style>
+</head>
+<body>
+<div class="row fullWidth">
+  <div class="large-12 columns">
+    <nav class="top-bar navBar" data-topbar>
+      <ul class="title-area">  
+        <li class="name">
+          <h1>
+            <a href="#">
+            E's Blue Notes
+            </a>
+          </h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+        <ul class="right">
+          <li><a href="../index.html">Intro</a></li>
+          <li><a href="../about.html">About</a></li>
+          <li><a href="../selling.php">Selling</a></li>
+          <li><a href="../pricelist.php">Prices</a></li>
+          <li><a href="../contact.html"><span id="current">Contact</span></a></li>
+        </ul>
+      </section>
+    </nav>
+  </div>
+</div>
+
+<div id="errors">
 <?php
 
 //Ensures no one loads page and does simple spam check.
@@ -31,13 +84,13 @@ if(isset($_POST['name']) && empty($_POST['spam_check']))
 			continue;
 		}
 		else {
-			$errors[] = $key . ' cannot be left blank';
+			$errors[] = $key . ' cannot be left blank. <br />';
 		}
 	}
 	
 	//Make sure the email is valid. 
     if (!$validator->check_email_address($input_email)) {
-           $errors[] = 'Email address is invalid.';
+           $errors[] = '<br /> Email address is invalid.';
     }
 	
 	//Now check to see if there are any errors 
@@ -47,11 +100,11 @@ if(isset($_POST['name']) && empty($_POST['spam_check']))
 		//No errors, send mail using conditional to ensure it was sent.
 		if(mail(webmaster_email, "Message from $input_name", $input_message, "From: $input_email"))
 		{
-		echo 'Your email has been sent.';
+		echo '<br /> Thanks! Your email has been sent.';
 		}
 		else 
 		{
-			echo 'There was a problem sending your email. Please try again.';
+			echo '<br /> There was a problem sending your email. Please try again.';
 		}
 		
 	}
@@ -65,5 +118,14 @@ if(isset($_POST['name']) && empty($_POST['spam_check']))
 }
 else
 {
-	die('Direct access to this page is not allowed.');
+	die('<br /> Direct access to this page is not allowed.');
 }
+
+?>
+</div>
+
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<script src="../bower_components/foundation/js/foundation.min.js"></script>
+<script src="../js/app.js"></script>
+</body>
+</html>
