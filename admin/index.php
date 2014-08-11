4319fe2c4a2bc84e39c83c947fc48667de98059c
tronicboone <e@sixtrill.com>
2014-08-11 12:58:21 -0400

    Fix incorrect link in nav menu

--- admin/index.php
+++ admin/index.php
@@ -28,7 +28,7 @@
         <ul class="right">
           <li><a href="../index.html">Intro</a></li>
           <li><a href="../about.html">About</a></li>
-          <li><a href="../selling.php">For Sale</a></li>
+          <li><a href="../forsale.php">For Sale</a></li>
           <li><a href="../contact.html">Contact</a></li>
         </ul>
       </section>
